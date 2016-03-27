9dfb74c056b836f48a9435ffb02af2a5d30c4082
Alexandes Heiets <sanomchik@gmail.com>
2016-03-27 16:14:37 +0300

    test

--- js/main.js
+++ js/main.js
@@ -1,29 +1,14 @@
-var a 
 $(document).ready(function () {
-  $('#get-data').click(function () {
-    var showData = $('#show-data');
+ var d = new Date();
+ var day = d.getDay();
 
-    $.getJSON('js/data.json', function (data) {
-      console.log(data.timing.first_week.monday);
-      console.log(data.timing.first_week);
-      console.log(data.timing);
-
-      var timing = data.timing.first_week.monday.map(function (item) {
-        console.log(item.name);
-        console.log(item.time);
-        return item.name + ': ' + item.time;
-      });
-      return a = data
-
-      showData.empty();
-
-      if (timing.length) {
-        var content = '<li>' + timing.join('</li><li>') + '</li>';
-        var list = $('<ul />').html(content);
-        showData.append(list);
-      }
-    });
-
-    showData.text('Loading the JSON file.');
+ $.getJSON('js/data.json', function (data) {
+  var timing = data.timing.first_week[day].map(function (item) {
+    console.log(item.name);
+    console.log(item.time);
+    return item.name + ': ' + item.time;
   });
+});
+
+ showData.text('Loading the JSON file.');
 });
