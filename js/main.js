d19ce23813e93097d97e6f6ba3a7cbf3b2eff276
Alexandes Heiets <sanomchik@gmail.com>
2016-03-27 16:17:10 +0300

    test

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
  var d = new Date();
  var day = d.getDay();
-
+var a = 'monday'
  $.getJSON('js/data.json', function (data) {
-  var timing = data.timing.first_week[2].map(function (item) {
+  var timing = data.timing.first_week[a].map(function (item) {
     console.log(item.name);
     console.log(item.time);
     return item.name + ': ' + item.time;
