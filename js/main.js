633a6cc6687a4ef9d81b63cf15f37e8ff2880134
Alexandes Heiets <sanomchik@gmail.com>
2016-03-27 14:06:29 +0300

    test

--- js/main.js
+++ js/main.js
@@ -5,9 +5,17 @@ $(document).ready(function () {
     $.getJSON('js/data.json', function (data) {
       console.log(data);
 
-     
+      var items = data.items.map(function (week) {
+        return week.first_week + ': ' + week.first_week.monday;
+      });
 
-      
+      showData.empty();
+
+      if (items.length) {
+        var content = '<li>' + items.join('</li><li>') + '</li>';
+        var list = $('<ul />').html(content);
+        showData.append(list);
+      }
     });
 
     showData.text('Loading the JSON file.');
