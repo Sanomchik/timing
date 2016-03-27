ffbe3fb87f1bcb0f315dcb7752e3c98853c31e1b
Alexandes Heiets <sanomchik@gmail.com>
2016-03-27 14:22:33 +0300

    test

--- js/main.js
+++ js/main.js
@@ -3,7 +3,10 @@ $(document).ready(function () {
     var showData = $('#show-data');
 
     $.getJSON('js/data.json', function (data) {
-      console.log(data);
+      console.log(data.timing.first_week.monday.first);
+      console.log(data.timing.first_week.monday);
+      console.log(data.timing.first_week);
+      console.log(data.timing);
 
       var timing = data.timing.first_week.monday.first.map(function (item) {
         return item.name + ': ' + item.time;
